4b8d2892e0b1ba5016dfec77ce046039ea89a004
edgetrit <95095924+edgetrit@users.noreply.github.com>
2021-11-27 17:22:06 +0100

    updated main to handle keyboard and map events

--- slightwm.py
+++ slightwm.py
@@ -1,6 +1,9 @@
 from Xlib import X, XK
 from Xlib.display import Display
 
+from keyboard import keyboard
+from map import mapping
+
 class winman:
     def __init__(self):
         self.display = Display()
@@ -8,14 +11,20 @@ class winman:
 
         self.rootWin.change_attributes(event_mask = X.SubstructureRedirectMask)
 
+        self.keyboardHandler = keyboard(self.display, self.rootWin)
+        self.mappingHandler = mapping(self.display, self.rootWin)
+
     def eventHandler(self):
         if self.display.pending_events() > 0:
             event = self.display.next_event()
             print("Event: ".format(str(event.type)))
+            if event.type == X.KeyPress: self.keyboardHandler.handleKeyEvent(event)
+            elif event.type == X.MapRequest: self.mappingHandler.mapHandler(event)
 
     def loop(self):
         while True:
             self.eventHandler()
+            self.updateFocus()
 
 windowManager = winman()
 windowManager.loop()
